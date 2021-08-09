010780296f8e130667211d1dada636cf76ad0205
imtyM <m.imtiazmukadam@gmail.com>
2021-08-09 19:49:48 +0200

    Add: Bucket spec

---
+++ lib/kv/bucket.ex
@@ -0,0 +1,24 @@
+defmodule KV.Bucket do
+  use Agent
+
+  @doc """
+  Starts a new Bucket
+  """
+  def start_link(_opts) do
+    Agent.start_link(fn -> %{} end)
+  end
+
+  @doc """
+  Gets a value from the Bucket by "KEY"
+  """
+  def get(bucket, key) do
+    Agent.get(bucket, &Map.get(&1, key))
+  end
+
+  @doc """
+  Puts the value for a given ket into the bucket
+  """
+  def put(bucket, key, value) do
+    Agent.update(bucket, &Map.put(&1, key, value))
+  end
+end
