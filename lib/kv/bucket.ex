5395442d1f5207516a607c4dd2eed814582f8d82
imtyM <m.imtiazmukadam@gmail.com>
2021-08-10 17:03:59 +0200

    Add: Delete function for the bucket

--- lib/kv/bucket.ex
+++ lib/kv/bucket.ex
@@ -12,13 +12,23 @@ defmodule KV.Bucket do
   Gets a value from the Bucket by "KEY"
   """
   def get(bucket, key) do
-    Agent.get(bucket, &Map.get(&1, key))
+    bucket
+    |> Agent.get(&Map.get(&1, key))
   end
 
   @doc """
   Puts the value for a given ket into the bucket
   """
   def put(bucket, key, value) do
-    Agent.update(bucket, &Map.put(&1, key, value))
+    bucket
+    |> Agent.update(&Map.put(&1, key, value))
+  end
+
+  @doc """
+  Deletes key from bucket, returns the current value of key
+  """
+  def delete(bucket, key) do
+    bucket
+    |> Agent.get_and_update(&Map.pop(&1, key))
   end
 end
