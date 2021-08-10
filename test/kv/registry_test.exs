d79bbd580c48c0b406f89c2150124993f95fdec6
imtyM <m.imtiazmukadam@gmail.com>
2021-08-10 19:42:22 +0200

    Add: Test that the registry deletes a bucket when stoped

--- test/kv/registry_test.exs
+++ test/kv/registry_test.exs
@@ -15,4 +15,13 @@ defmodule KV.RegistryTest do
     bucket |> KV.Bucket.put("milk", 1)
     assert bucket |> KV.Bucket.get("milk") == 1
   end
+
+  test "removes buckets on exit", %{registry: registry} do
+    KV.Registry.create(registry, "shopping")
+    {:ok, bucket} = KV.Registry.lookup(registry, "shopping")
+
+    Agent.stop(bucket)
+
+    assert KV.Registry.lookup(registry, "shopping") == :error
+  end
 end
